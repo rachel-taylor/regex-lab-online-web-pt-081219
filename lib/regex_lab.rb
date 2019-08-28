e18f3a20171de16a3e5c79f25167e7517ecdcee2
Rachel T <rachmaybe@gmail.com>
2019-08-28 03:41:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -30,4 +30,10 @@ if text.match(/^[A-Z]/) && text.match(/[\.]$/)
   else 
     false
   end
+  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
+end
 end

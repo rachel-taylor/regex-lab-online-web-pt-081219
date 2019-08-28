5cf45942236689c5a07a7f4b6eb141ae69c60751
Rachel T <rachmaybe@gmail.com>
2019-08-28 03:41:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -30,10 +30,12 @@ if text.match(/^[A-Z]/) && text.match(/[\.]$/)
   else 
     false
   end
+end
+
+def valid_phone_number?(phone)
   if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
     true
   else
     false
   end
-end
 end
